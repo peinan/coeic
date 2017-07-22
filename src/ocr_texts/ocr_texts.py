1ab82a95a384aa4bd513c2fd210bb615a0116cfe
peinan <peinan7@gmail.com>
2017-07-22 14:56:30 +0900

    :tada: minor fixes and added sample images

--- src/ocr_texts/ocr_texts.py
+++ src/ocr_texts/ocr_texts.py
@@ -5,50 +5,6 @@
 # Author:     Peinan ZHANG
 # Created at: 2017-07-22
 
-"""
-IN
-===
-extracted_balloons = {
-  "upload_img_path": "IMG_PATH",
-  "splited_frames": [
-    {
-      "frame_img": "frame_img_01",
-      "extracted_balloons": [
-        "balloon_img1",
-        "balloon_img2",
-        ...
-      ]
-    },
-    ...
-  ]
-}
-
-OUT
-====
-ocred_texts = {
-  "upload_img_path": "IMG_PATH",
-  "splited_frames": [
-    {
-      "frame_img": "frame_img_01",
-      "extracted_balloons": [
-        {
-          "balloon_img": "balloon_img1",
-          "texts": {
-            "text": "serif",
-            "position": {
-              "left_upper": [x, y],
-              "right_bottom": [x, y],
-            }
-          }
-        },
-        ...
-      ]
-    },
-    ...
-  ]
-}
-"""
-
 import json
 import requests, io, os
 
@@ -65,32 +21,52 @@ from google.cloud import vision
 
 
 class OcrTexts:
+  CURRENT_DIR = os.getcwd()
+  FRAME_IMG_DIR = 'frames'
+  BALLOON_IMG_DIR = 'balloons'
+
+
   def __init__(self, in_json_fp):
     self.extracted_balloons = self.parse_input(in_json_fp)
-    self.main(extracted_balloons)
+    # print('[DEBUG] INPUT:', self.extracted_balloons)
 
 
   def main(self):
     # certification
     self.certify_google_api()
 
+    img_root_dir = os.path.split(self.extracted_balloons['upload_img_path'])[0]
     frames = self.extracted_balloons['splited_frames']
-    result_frames = []
+
+    frame_results = []
     for frame in frames:
+      balloon_results = []
       for balloon in frame['extracted_balloons']:
-        ocr_result = self.ocr_image(balloon)
+        balloon_fp = os.path.join(self.CURRENT_DIR,\
+                                  img_root_dir,\
+                                  self.BALLOON_IMG_DIR,\
+                                  balloon)
+        ocr_result = self.ocr_image(balloon_fp)
         result = self.parse_ocr_result(ocr_result)
-        frame['texts'] = result
-        result_frames.append(frame)
+
+        balloon_result = {'balloon_img': balloon}
+        balloon_result['texts'] = result
+        balloon_results.append(balloon_result)
+
+        # print('[DEBUG] frame {} ballon {}'.format(frame['frame_img'], balloon))
+        # print('[DEBUG] result:', result)
+
+      frame_results.append({'frame_img': frame['frame_img'],\
+                            'extracted_balloons': balloon_results})
 
     ocred_texts = self.extracted_balloons
-    ocred_texts['splited_frames'] = result_frames
+    ocred_texts['splited_frames'] = frame_results
 
     self.output_result(ocred_texts)
 
 
   def parse_input(self, in_json_fp):
-    in_json = json.load(open(in_json_fp), 'r')
+    in_json = json.load(open(in_json_fp, 'r'))
 
     return in_json
 
@@ -100,17 +76,18 @@ class OcrTexts:
     self.vision_client = vision.Client()
 
 
-  def ocr_image(self, balloon):
+  def ocr_image(self, balloon_fp):
     # Loads a image into memory
-    with io.open(balloon, 'rb') as img_file:
+    with io.open(balloon_fp, 'rb') as img_file:
       content = img_file.read()
-      img = vision_client.image(content=content)
+      img = self.vision_client.image(content=content)
       texts = img.detect_text()
 
     return texts
 
 
   def parse_ocr_result(self, ocr_result):
+    text = ocr_result[0]
     description = text.description
     left_upper, right_upper, right_bottom, left_bottom = text.bounds.vertices
 
@@ -126,10 +103,11 @@ class OcrTexts:
 
 
   def output_result(self, ocred_texts):
-    json.dumps(ocred_texts, ensure_ascii=False)
+    # print('RESULT\n======')
+    print(json.dumps(ocred_texts, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-  json_fp = ''
-  ocr = OcrTexts('')
+  json_fp = 'dummy_extracted_balloons.json'
+  ocr = OcrTexts(json_fp)
   ocr.main()
