d2e0cd7682e808a3d6364f1b99e6f9eb613e27d3
Junichi Sato <jsato8094@gmail.com>
2017-07-23 16:46:17 +0900

    GET機能実装など

--- src/storage/app.js
+++ src/storage/app.js
@@ -4,15 +4,31 @@ if (process.argv.length < 3) {
   throw Error("Usage: " + process.argv.join(" ") + " <storage_root_path>")
 }
 
+// 実際に保存されるストレージのルート
 const storage_root = process.argv[2].endsWith("/") ? process.argv[2] : process.argv[2] + "/";
-const upload_img_root = storage_root + "uploaded_imgs/";
-const processed_img_root = storage_root + "processed_imgs/";
-const voice_root = storage_root + "voice/";
 
-const base_url = "http://104.155.222.216:5000/api/";
-const upload_img_url = base_url + "uploadedImg/";
-const processed_img_url = base_url + "processedImg/";
-const voice_url = base_url + "voice/";
+// それぞれの相対パス
+const uploaded_img_relative_path = "uploaded_imgs/";
+const processed_img_relative_path = "processed_imgs/";
+const voice_relative_path = "voices/";
+
+// それぞれの保存先
+const uploaded_img_root = storage_root + uploaded_img_relative_path;
+const processed_img_root = storage_root + processed_img_relative_path;
+const voice_root = storage_root + voice_relative_path;
+
+// APIのベースURL
+const base_url = "http://104.155.222.216:5000/";
+
+// 実際の画像にアクセスするためのURL
+const uploaded_img_path_url = base_url + uploaded_img_relative_path;
+const processed_img_path_url = base_url + processed_img_relative_path;
+const voice_path_url = base_url + voice_relative_path;
+
+// それぞれの情報を問い合わせるためのAPIのパス
+const uploaded_img_api_url = base_url + "api/uploadedImg/";
+const processed_img_api_url = base_url + "api/processedImg/";
+const voice_api_url = base_url + "api/voice/";
 
 const express = require('express');
 const app = express();
@@ -30,7 +46,7 @@ const db = require('./db');
 const multer = require('multer');
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, upload_img_root);
+    cb(null, uploaded_img_root);
   },
   filename: function (req, file, cb) {
     cb(null, new Date().getTime() + ".jpg");
@@ -48,7 +64,7 @@ app.post("/api/uploadedImg", upload.single('file'), function (req, res) {
   try {
     // 最新の画像ファイル名を取得
     const filename =
-      Math.max.apply(null, walkSync(upload_img_root).map(function (name, index, array) {
+      Math.max.apply(null, walkSync(uploaded_img_root).map(function (name, index, array) {
       return name.split(".")[0];
     })).toString() + '.jpg';
 
@@ -61,7 +77,7 @@ app.post("/api/uploadedImg", upload.single('file'), function (req, res) {
           status: "success",
           result: {
             id: result.id,
-            url: upload_img_url + result.filename,
+            url: uploaded_img_path_url + result.filename,
             status: result.status,
             created_at: result.created_at,
             updated_at: result.updated_at
@@ -75,7 +91,6 @@ app.post("/api/uploadedImg", upload.single('file'), function (req, res) {
     });
 
   } catch (e) {
-    // todo: どこかで失敗したらエラーレスポンスを返す
     console.log(e);
     res.send({
       status: "failure",
@@ -92,11 +107,6 @@ app.post("/api/uploadedImg", upload.single('file'), function (req, res) {
  * <a href="https://github.com/peinan/coeic/wiki/API%E4%BB%95%E6%A7%98_GET:uploadedImg">wiki</a>
  */
 app.get("/api/uploadedImg", function (req, res) {
-  const walkSync = require('walk-sync');
-  const fs = require('fs');
-
-  var result;
-
   try {
     const results = typeof req.body["results"] !== 'undefined' ? req.body["results"] : 0;
 
@@ -104,86 +114,76 @@ app.get("/api/uploadedImg", function (req, res) {
     const max_num = results === 0 ? 65535 : results;
 
     // DBから画像情報取得
-
-
-    // todo:  DBからファイル情報取得．今はダミー実装
-    var paths = walkSync(upload_img_root);
-    paths = paths.slice(0, max_num + 1);
-    const info_list = paths.map(function (name, index, array) {
-      const stat = fs.statSync(upload_img_root + name);
-      return {
-        id: parseInt(name.split(".")[0]),
-        url: upload_img_url + name,
-        status: "done",
-        created_at: stat.birthtime,
-        updated_at: stat.ctime
-      };
+    db.selectMulti(max_num).then(function (results) {
+      const info_list = results.map(function (r) {
+        return {
+          id: r.id,
+          url: uploaded_img_path_url + r.filename,
+          status: r.status,
+          created_at: r.created_at,
+          updated_at: r.updated_at
+        }
+      });
+      res.send({
+        status: "success",
+        result: info_list
+      })
+    }).catch(function (err) {
+      throw err;
     });
-
-    result = {
-      status: "success",
-      result: info_list
-    }
-
   } catch (e) {
-    result = {
+    console.log(e);
+    res.send({
       status: "failure",
       result: {
         message: "画像取得に失敗しました"
       }
-    }
+    });
   }
-
-  res.send(result);
 });
 
 /**
  * get the specified uploaded image
  * <a href="https://github.com/peinan/coeic/wiki/API%E4%BB%95%E6%A7%98_GET:uploadedImg">wiki</a>
  */
-app.get("/api/uploadedImg/:id", function (req, res) {
-
-  const walkSync = require('walk-sync');
-  const fs = require('fs');
-
-  var result;
-
+app.get("/api/uploadedImg/:id", function (req, res, next) {
   try {
-    var paths = walkSync(upload_img_root);
-    const info_list = paths.filter(function (name) {
-      return parseInt(name.split(".")[0]) === parseInt(req.params.id);
-    }).map(function (name, index, array) {
-      // todo:  DBからファイル情報取得．今はダミー実装
-      const stat = fs.statSync(upload_img_root + name);
-      return {
-        id: parseInt(name.split("/")[0]),
-        url: upload_img_url + name,
-        status: "done",
-        created_at: stat.birthtime,
-        updated_at: stat.ctime
-      };
+    // DBから画像情報取得
+    db.select(req.params.id).then(function (results) {
+      const info_list = results.map(function (r) {
+        return {
+          id: r.id,
+          url: uploaded_img_path_url + r.filename,
+          status: r.status,
+          created_at: r.created_at,
+          updated_at: r.updated_at
+        }
+      });
+      if (info_list.length === 0) {
+        res.send({
+          status: "failure",
+          result: {
+            message: "画像が存在しません"
+          }
+        });
+        next();
+      }
+      res.send({
+        status: "success",
+        result: info_list[0]
+      })
+    }).catch(function (err) {
+      throw err;
     });
-
-    if (info_list.length === 0) {
-      throw Error();
-    }
-    const info = info_list[0];
-
-    result = {
-      status: "success",
-      result: info
-    }
-
   } catch (e) {
-    result = {
+    console.log(e);
+    res.send({
       status: "failure",
       result: {
         message: "画像取得に失敗しました"
       }
-    }
+    });
   }
-
-  res.send(result);
 });
 
 /**
@@ -199,7 +199,7 @@ app.get("/api/processedImg/:id", function (req, res) {
   try {
     var paths = walkSync(processed_img_root + req.params.id);
     paths = paths.map(function (name) {
-      return processed_img_url + req.params.id + "/" + name;
+      return processed_img_path_url + req.params.id + "/" + name;
     });
     result = {
       status: "success",
@@ -222,7 +222,7 @@ app.get("/api/processedImg/:id", function (req, res) {
  * get the processed image
  * <a href="https://github.com/peinan/coeic/wiki/API%E4%BB%95%E6%A7%98_GET:processedImg">wiki</a>
  */
-app.get("/api/processedImg/:id/:name", function (req, res) {
+app.get(processed_img_relative_path + ":id/:name", function (req, res) {
 
   const fileExists = require('file-exists');
 
@@ -259,7 +259,7 @@ app.get("/api/voice/:id", function (req, res) {
   try {
     var paths = walkSync(voice_root + req.params.id);
     paths = paths.map(function (name) {
-      return voice_url + req.params.id + "/" + name;
+      return voice_path_url + req.params.id + "/" + name;
     });
     result = {
       status: "success",
@@ -282,7 +282,7 @@ app.get("/api/voice/:id", function (req, res) {
  * get the voice file
  * <a href="https://github.com/peinan/coeic/wiki/API%E4%BB%95%E6%A7%98_GET:voice">wiki</a>
  */
-app.get("/api/voice/:id/:name", function (req, res) {
+app.get(voice_relative_path + ":id/:name", function (req, res) {
 
   const fileExists = require('file-exists');
 
