89dc8160dc0f483f07da55f8d688bd302481cfd1
Junichi Sato <jsato8094@gmail.com>
2017-07-23 18:17:22 +0900

    ヘッダ付与

--- src/storage/app.js
+++ src/storage/app.js
@@ -36,7 +36,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
  */
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Cache-Control');
   res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Max-Age', '86400');
