6e026decf9b087c36089d3e918047ddb1229a902
Junichi Sato <jsato8094@gmail.com>
2017-07-23 15:47:31 +0900

    画像アップロード機能追加

--- src/storage/app.js
+++ src/storage/app.js
@@ -14,17 +14,82 @@ const upload_img_url = base_url + "uploadedImg/";
 const img_url = base_url + "processedImg/";
 const voice_url = base_url + "voice/";
 
-const express = require("express");
+const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const server = app.listen(5000, function () {
   console.log("Node.js is listening to PORT:" + server.address().port);
 });
 
-/** get all of uploaded images
+const db = require('./db');
+
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, upload_img_root);
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().getTime() + ".jpg");
+  },
+  preservePath: new Date().getTime() + ".jpg"
+});
+const upload = multer({storage: storage});
+
+/**
+ * upload image
+ * https://github.com/peinan/coeic/wiki/API%E4%BB%95%E6%A7%98_POST:uploadedImg
+ */
+app.post("/api/uploadedImg", upload.single('file'), function (req, res) {
+  const walkSync = require('walk-sync');
+
+  try {
+    // 最新の画像ファイル名を取得
+    const filename =
+      Math.max.apply(null, walkSync(upload_img_root).map(function (name, index, array) {
+      return name.split(".")[0];
+    })).toString() + '.jpg';
+
+    // DBにデータ追加
+    db.insert(filename, db.STATUS_TODO, '').then(function (results) {
+      // 追加した最新のデータを返す
+      db.selectMulti(1).then(function (results) {
+        const result = results[0];
+        res.send({
+          status: "success",
+          result: {
+            id: result.id,
+            url: upload_img_url + result.filename,
+            status: result.status,
+            created_at: result.created_at,
+            updated_at: result.updated_at
+          }
+        })
+      }).catch(function (err) {
+        throw err;
+      })
+    }).catch(function (err) {
+      throw err;
+    });
+
+  } catch (e) {
+    // todo: どこかで失敗したらエラーレスポンスを返す
+    console.log(e);
+    res.send({
+      status: "failure",
+      result: {
+        message: "画像のアップロードに失敗しました"
+      }
+    });
+  }
+});
+
+
+/**
+ * get all of uploaded images
  * <a href="https://github.com/peinan/coeic/wiki/API%E4%BB%95%E6%A7%98_GET:uploadedImg">wiki</a>
  */
 app.get("/api/uploadedImg", function (req, res) {
@@ -39,10 +104,13 @@ app.get("/api/uploadedImg", function (req, res) {
     // body.resultsが未指定または0の場合は全ての画像を取得（とはいえ上限を設けておく）
     const max_num = results === 0 ? 65535 : results;
 
+    // DBから画像情報取得
+
+
+    // todo:  DBからファイル情報取得．今はダミー実装
     var paths = walkSync(upload_img_root);
     paths = paths.slice(0, max_num + 1);
     const info_list = paths.map(function (name, index, array) {
-      // todo:  DBからファイル情報取得．今はダミー実装
       const stat = fs.statSync(upload_img_root + name);
       return {
         id: parseInt(name.split(".")[0]),
@@ -70,7 +138,8 @@ app.get("/api/uploadedImg", function (req, res) {
   res.send(result);
 });
 
-/** get the specified uploaded image
+/**
+ * get the specified uploaded image
  * <a href="https://github.com/peinan/coeic/wiki/API%E4%BB%95%E6%A7%98_GET:uploadedImg">wiki</a>
  */
 app.get("/api/uploadedImg/:id", function (req, res) {
@@ -118,7 +187,8 @@ app.get("/api/uploadedImg/:id", function (req, res) {
   res.send(result);
 });
 
-/** find processed images
+/**
+ * find processed images
  * <a href="https://github.com/peinan/coeic/wiki/API%E4%BB%95%E6%A7%98_GET:processedImg">wiki</a>
  */
 app.get("/api/processedImg/:id", function (req, res) {
@@ -149,7 +219,8 @@ app.get("/api/processedImg/:id", function (req, res) {
   res.send(result);
 });
 
-/** get the processed image
+/**
+ * get the processed image
  * <a href="https://github.com/peinan/coeic/wiki/API%E4%BB%95%E6%A7%98_GET:processedImg">wiki</a>
  */
 app.get("/api/processedImg/:id/:name", function (req, res) {
@@ -176,7 +247,8 @@ app.get("/api/processedImg/:id/:name", function (req, res) {
   });
 });
 
-/** find voice files
+/**
+ * find voice files
  * <a href="https://github.com/peinan/coeic/wiki/API%E4%BB%95%E6%A7%98_GET:voice">wiki</a>
  */
 app.get("/api/voice/:id", function (req, res) {
@@ -207,7 +279,8 @@ app.get("/api/voice/:id", function (req, res) {
   res.send(result);
 });
 
-/** get the voice file
+/**
+ * get the voice file
  * <a href="https://github.com/peinan/coeic/wiki/API%E4%BB%95%E6%A7%98_GET:voice">wiki</a>
  */
 app.get("/api/voice/:id/:name", function (req, res) {
